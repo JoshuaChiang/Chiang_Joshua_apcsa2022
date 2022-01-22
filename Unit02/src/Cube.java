6df2ae623c0d6a7849adbf896614de2c7a6c7eaf
JoshuaChiang <97621342+JoshuaChiang@users.noreply.github.com>
2022-01-21 21:17:46 -0800

    final push of Unit02

---
+++ Unit02/src/Cube.java
@@ -0,0 +1,13 @@
+//(c) A+ Computer Science
+// www.apluscompsci.com
+//Name - Joshua Chiang
+//Date - 1/21/2022
+
+public class Cube
+{
+	public static double area( int side )
+	{
+		int area = 6*side*side;
+		return area;
+	}
+}
