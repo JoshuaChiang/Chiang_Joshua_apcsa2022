3fe03b7a01c7a1aa4ee3d7ed537c7edb2fe13d46
JoshuaChiang <97621342+JoshuaChiang@users.noreply.github.com>
2022-03-09 00:07:11 -0800

    update unit 12

--- Unit12/src/Word.java
+++ Unit12/src/Word.java
@@ -7,33 +7,25 @@ import static java.lang.System.*;
 public class Word implements Comparable<Word>
 {
 	private String word;
-
+	
 	public Word( String s )
 	{
-		this.word=s;
+		word = s;
 	}
 
-	public String getWord()
-	{
-		return word;
-	}
-	
 	public int compareTo( Word rhs )
 	{		
-		if (this.getWord().length()< rhs.getWord().length())
-		return -2;
-		
-		
-		else if (this.getWord().length()>rhs.getWord().length())
-				return 2;
-		else
-		{
-			return this.getWord().compareTo(rhs.getWord());
+		if (word.length() == rhs.word.length()) {
+			return word.compareTo(rhs.word);
 		}
+		else if (word.length() > rhs.word.length()) {
+			return 1;
 		}
+		else return -1;
+	}
 
 	public String toString()
 	{
-		return "";
+		return word;
 	}
 }
--- Unit12/src/WordRunner.java
+++ Unit12/src/WordRunner.java
@@ -15,36 +15,20 @@ public class WordRunner
 	public static void main( String args[] ) throws IOException
 	{
 		Scanner file = new Scanner(new File("words.dat"));
-
+		
 		int size = file.nextInt();
 		file.nextLine();
-		
-		ArrayList<Word> words = new ArrayList<Word>();
-		
-		
-		while (file.hasNext())
-		{
-			words.add(new Word(file.next()));
+
+		Word[] words = new Word[size];
+		for (int i = 0; i < words.length; i++) {
+			words[i] = new Word(file.nextLine());
+			
 		}
 		
-		Word[] wordss = words.toArray(new Word[0]);
-		Collections.sort(words);
+		Arrays.sort(words);
 		
-		
-		for(Word ww:words)
-		{
-			System.out.println(ww.getWord());
+		for (Word word : words) {
+			out.println(word);
 		}
-
-
-
-
-
-
-
-
-
-
-
 	}
 }
